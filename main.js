8530f2ede36e85e6129af7e85306f0f5612e2e6f
Aaron Shanks <aaronshanks3@gmail.com>
2022-11-22 16:49:44 -0600

    created and started JS file

---
+++ main.js
@@ -0,0 +1,6 @@
+const inputDisplay = document.getElementsByClassName("inputDisplay");
+const allButtons = document.querySelectorAll(".buttons"
+);
+const clearAllButton = document.getElementById("clearAll");
+const deleteButton = document.getElementById("deleteOne");
+const equalButton = document.getElementById("equalButton");
