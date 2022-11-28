b65600f395e5337c6fab1dc561e9202656e59d4e
Aaron Shanks <aaronshanks3@gmail.com>
2022-11-28 16:36:07 -0600

    Updated input tag with DIV. button values appear

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-const inputDisplay = document.getElementsByClassName("inputDisplay");
+const inputDisplay = document.querySelector(".inputDisplay");
 const allButtons = document.querySelectorAll(".buttons"
 );
-const clearAllButton = document.getElementById("clearAll");
-const deleteButton = document.getElementById("deleteOne");
-const equalButton = document.getElementById("equalButton");
+const clearAllButton = document.querySelector("#clearAll");
+const deleteButton = document.querySelector("#deleteOne");
+const equalButton = document.querySelector("#equalButton");
+
+//Target the event. Pull the text from event. Display the text from event into the input field
+
+allButtons.forEach(buttons => {
+  buttons.addEventListener("click", (event) => {
+    const pressedButton = event.target;
+    const buttonValue = pressedButton.textContent;
+    inputDisplay.innerHTML = buttonValue
+
+    })
+})
