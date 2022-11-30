843bfd236cde6b3ec7f6f2e6959ab3501f01efe4
Aaron Shanks <aaronshanks3@gmail.com>
2022-11-30 03:52:43 -0600

    got second number to display after operator

--- main.js
+++ main.js
@@ -2,16 +2,21 @@ const inputDisplay = document.querySelector(".inputDisplay");
 const allButtons = document.querySelectorAll(".buttons"
 );
 const clearAllButton = document.querySelector("#clearAll");
-const deleteButton = document.querySelector("#deleteOne");
 const equalButton = document.querySelector("#equalButton");
+const operatorButton = document.querySelector(".operator");
 
 //Target the event. Pull the text from event. Display the text from event into the input field
 
 allButtons.forEach(buttons => {
-  buttons.addEventListener("click", (event) => {
+  buttons.addEventListener("click", event => {
+    // if (!event.target.closest("button")) { 
     const pressedButton = event.target;
     const buttonValue = pressedButton.textContent;
-    inputDisplay.innerHTML = buttonValue
+    const displayValue = inputDisplay.textContent
 
-    })
-})
+    if(displayValue === "0"){
+    inputDisplay.textContent = buttonValue}
+    else {inputDisplay.textContent = displayValue + buttonValue}
+    // Number Concatination Works
+
+})})
