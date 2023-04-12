b28f5a80360ae7439d462b08ea18294abd9cb3b3
Adshanks <aaronshanks3@gmail.com>
2023-04-12 11:22:08 -0500

    added functionality to clear button

--- main.js
+++ main.js
@@ -15,47 +15,44 @@ allButtons.forEach(buttons => {
     const displayValue = inputDisplay.textContent
     const {previousKeyType} = calculator.dataset
 
-    // When Number Button is Pressed
-   if(pressedButton.classList.contains("number"))
-    {
-      if(displayValue === "0")
-      {
-    inputDisplay.textContent = buttonValue}
-      else if(previousKeyType === "operator")
-     {
-       inputDisplay.textContent = buttonValue
-     }
-      else {inputDisplay.textContent = displayValue + buttonValue
-    }
+  // When Number Button is Pressed
+if(pressedButton.classList.contains("number"))
+  {
+    if(displayValue === "0")
+      {inputDisplay.textContent = buttonValue}
+    else if(previousKeyType === "operator")
+     {inputDisplay.textContent = buttonValue}
+    else {inputDisplay.textContent = displayValue + buttonValue}
   }
   calculator.dataset.previousKeyType = "number"
-
-     // When Operator Button is Pressed
-if(pressedButton.classList.contains("operator")) {
-  
-   calculator.dataset.previousKeyType = "operator"
+// When Operator Button is Pressed
+if(pressedButton.classList.contains("operator")) 
+ {
+  calculator.dataset.previousKeyType = "operator"
   calculator.dataset.firstNumber = displayValue;
   calculator.dataset.operator = pressedButton.dataset.key
-  
-};
+ };
     //Calculate
  if(pressedButton.classList.contains("equal"))
  {
-   const firstNumber = parseInt(calculator.dataset.firstNumber);
+   const firstNumber = parseFloat(calculator.dataset.firstNumber);
    const operator = calculator.dataset.operator;
-   const secondNumber = parseInt(displayValue);
-      
-      let result = ""
+   const secondNumber = parseFloat(displayValue);
+  
+   let result = ""
    if(operator === "add") {result = firstNumber + secondNumber};
    if(operator ==="subtract") {result = firstNumber - secondNumber};
    if(operator ==="multiply") {result = firstNumber * secondNumber};
    if(operator === "divide") {result = firstNumber / secondNumber};
-   
    inputDisplay.textContent = result
    calculator.dataset.previousKeyType = pressedButton.dataset
  }
- 
+ if(pressedButton.classList.contains("clearAll"))
+  {
+    inputDisplay.textContent = "0"
+    delete calculator.dataset.firstNumber
+    delete calculator.dataset.secondNumber
+  }
  })
-
-  })
+ })
 
